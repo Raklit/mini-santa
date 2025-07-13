36cb9955993feeba3bda43b01d85a7dfafb4a451
Raklit <raklitnewmail@gmail.com>
2025-07-13 16:31:24 +0800

    fix pool data model

--- src/santa/data_model/implementations/pool.rs
+++ src/santa/data_model/implementations/pool.rs
@@ -1,4 +1,4 @@
-use chrono::prelude::*;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 use crate::core::data_model::traits::{IAccountRelated, ILocalObject};
@@ -14,8 +14,6 @@ pub struct Pool {
     min_price : u64,
     max_price : u64,
     is_creator_involved : bool,
-    members : Vec<String>,
-    rooms : Vec<String>,
     creation_date : DateTime<Utc>,
     lifetime : u64,
     state : PoolState
@@ -35,7 +33,7 @@ impl IAccountRelated for Pool {
 }
 
 impl IPool for Pool {
-    fn new(id : &str, name : &str, description : &str, account_id : &str, min_price : u64, max_price : u64, is_creator_involved : bool, members : Vec<String>, rooms : Vec<String>, creation_date : DateTime<Utc>, lifetime : u64, state : PoolState) -> Self {
+    fn new(id : &str, name : &str, description : &str, account_id : &str, min_price : u64, max_price : u64, is_creator_involved : bool, creation_date : DateTime<Utc>, lifetime : u64, state : PoolState) -> Self {
         return Pool {
             id: String::from(id),
             name: String::from(name),
@@ -44,8 +42,6 @@ impl IPool for Pool {
             min_price: min_price,
             max_price: max_price,
             is_creator_involved: is_creator_involved,
-            members: members,
-            rooms: rooms,
             creation_date: creation_date,
             lifetime: lifetime,
             state: state
@@ -62,10 +58,6 @@ impl IPool for Pool {
 
     fn is_creator_involdved(&self) -> bool { self.is_creator_involved }
 
-    fn members(&self) -> Vec<String> { self.members.clone() }
-
-    fn rooms(&self) -> Vec<String> { self.rooms.clone() }
-
     fn creation_date(&self) -> DateTime<Utc> { self.creation_date }
 
     fn lifetime(&self) -> u64 { self.lifetime }
@@ -81,29 +73,7 @@ impl IPool for Pool {
     fn set_max_price(&mut self, max_price : u64) -> () { self.max_price = max_price; }
 
     fn set_is_creator_involved(&mut self, is_creator_involved : bool) -> () { self.is_creator_involved = is_creator_involved; }
-
-    fn set_members(&mut self, members : Vec<String>) -> () { self.members = members }
-
-    fn clear_members(&mut self) -> () { self.members.clear(); }
-
-    fn add_member(&mut self, member_id : &str) -> () { self.members.push(String::from(member_id)); }
-
-    fn delete_member(&mut self, member_id : &str) ->() {
-        let index = self.members.iter().position(|x| *x == member_id).unwrap();
-        self.members.remove(index);
-     }
-
-    fn set_rooms(&mut self, rooms : Vec<String>) -> () { self.rooms = rooms; }
-
-    fn clear_rooms(&mut self) { self.rooms.clear(); }
-
-    fn add_room(&mut self, room_id : &str) { self.rooms.push(String::from(room_id)); }
-
-    fn delete_room(&mut self, room_id : &str) {
-        let index = self.rooms.iter().position(|x| *x == room_id).unwrap();
-        self.rooms.remove(index);
-    }
-
+    
     fn set_creation_date(&mut self, creation_date : DateTime<Utc>) -> () { self.creation_date = creation_date; }
 
     fn set_lifetime(&mut self, lifetime : u64) -> () { self.lifetime = lifetime; }
